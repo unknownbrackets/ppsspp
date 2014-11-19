d3645f43a21203c34f5b9538d3e92ca6903bf6eb
Henrik Rydgard <hrydgard@gmail.com>
2014-11-20 00:52:48 +0100

    JIT IR: Skip already-compiled delay slots more elegantly than "-1". Solve the ReplaceJalTo problem.

--- Core/MIPS/x86/Jit.h
+++ Core/MIPS/x86/Jit.h
@@ -34,6 +34,8 @@
 #include "Core/MIPS/IR.h"
 
 class PointerWrap;
+struct ReplacementTableEntry;
+
 
 namespace MIPSComp {
 
@@ -178,6 +180,7 @@ public:
 
 private:
 	void GenerateFixedCode(JitOptions &jo);
+	void ExtractIR(u32 address, IRBlock *block);
 	void GetStateAndFlushAll(RegCacheState &state);
 	void RestoreState(const RegCacheState& state);
 	void FlushAll();
